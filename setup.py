a518f3b0a3fe72c2111c5b1b8122df2c8b4ab6ab
Rickard Dybeck <rickard.dybeck@klarna.com>
2012-11-05 10:42:46 +0100

    Digester added
        Also some minor fixes to the structure

--- setup.py
+++ setup.py
@@ -3,7 +3,10 @@ from distutils.core import setup
 setup(
     name='KlarnaCheckout',
     version='0.1dev',
-    packages=['klarnacheckout',],
+    packages=['klarnacheckout'],
     license='Apache 2',
     long_description=open('README.txt').read(),
+    author='Merchant Integration',
+    author_email='support@klarna.com',
+    url='http://integration.klarna.com'
 )
