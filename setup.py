4d0465784f6f458d082a3f181e22a18d7f40239c
Rickard Dybeck <rickard.dybeck@klarna.com>
2012-11-05 09:54:34 +0100

    Structure in place

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='KlarnaCheckout',
+    version='0.1dev',
+    packages=['klarnacheckout',],
+    license='Apache 2',
+    long_description=open('README.txt').read(),
+)
