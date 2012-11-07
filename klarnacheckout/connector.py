0420114cce5c265e6e5e423e6d48d00af4a693bc
David Keijser <david.keijser@klarna.com>
2012-11-07 14:06:23 +0100

    Add facade and update documentation

--- klarnacheckout/connector.py
+++ klarnacheckout/connector.py
@@ -1,4 +1,21 @@
-'''Connector'''
+'''Connector
+
+Defines a class by which facilitates performing HTTP actions on resources.
+'''
+
+# Copyright 2012 Klarna AB
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 __all__ = ('Connector',)
 
@@ -17,7 +34,13 @@ import json
 
 
 class Connector(object):
-    '''Basic connector'''
+    '''Basic connector
+
+    Uses a customised urllib(2) OpenerDirector to perform http requests while
+    making sure to update the local resource accordingly.
+
+    e.g updates location on HTTP 201
+    '''
 
     def __init__(self, useragent, digester, build=build_opener):
         self.opener = build(
--- klarnacheckout/useragent.py
+++ klarnacheckout/useragent.py
@@ -1,20 +1,26 @@
+'''UserAgent
+
+Generates a nicely formatted user agent string containing some general system
+information and the version of this library.
+
+Additional fields can be added with the formatting taken care of.
 '''
-UserAgent
 
-Copyright 2012 Klarna AB
+# Copyright 2012 Klarna AB
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-'''
+__version__ = '1.0.0'
 
 import sys
 import os
@@ -30,7 +36,7 @@ class UserAgent(object):
         self._fields = {
             "Library": {
                 "name": "Klarna.ApiWrapper",
-                "version": "1.0"
+                "version": __version__
             },
             "OS": {
                 "name": os.uname()[0],
