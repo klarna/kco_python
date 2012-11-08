04fd2edcd2c9937a568b3ba7f3c8672197b81ecd
Majid Garmaroudi <majid.garmaroudi@klarna.com>
2012-11-08 15:25:22 +0100

    Adding Example files
    Making checkout.py, confirmation.py and push.py
    validating them with pep8 and pyflakes beside running them
    in python to see if they work

---
+++ docs/examples/confirmation.py
@@ -0,0 +1,39 @@
+'''Confirmation Example
+
+This file demonstrates the use of the Klarna library to complete
+the purchase and display the confirmation page snippet.
+'''
+
+# Copyright 2012 Klarna AB
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import klarnacheckout
+
+# Instance of the session library that is being used in the server
+session = {}
+
+# Shared Secret
+shared_secret = 'shared_secret'
+
+connector = klarnacheckout.create_connector(shared_secret)
+
+checkout_id = session['klarna_checkout']
+order = klarnacheckout.Order()
+order.fetch(connector, checkout_id)
+
+if order['status'] != 'checkout_complete':
+    raise Exception('Checkout not completed, redirect to checkout.py')
+
+print "<div>%s</div>" % (order["gui"]["snippet"])
+del session['klarna_checkout']
---
+++ docs/examples/push.py
@@ -0,0 +1,46 @@
+'''Push Example
+
+This file demonstrates the use of the Klarna library to complete
+the purchase and create the order.
+'''
+
+# Copyright 2012 Klarna AB
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import klarnacheckout
+from uuid import uuid1
+
+# Instance of the session library that is being used in the server
+session = {}
+
+# Instance of the HTTP library that is being used in the server
+request = {}
+
+# Shared Secret
+shared_secret = 'shared_secret'
+
+connector = klarnacheckout.create_connector(shared_secret)
+
+checkout_id = request['checkout_uri']
+order = klarnacheckout.Order()
+order.fetch(connector, checkout_id)
+
+if order['status'] == 'checkout_complete':
+    # At this point make sure the order is created in your system and send a
+    # confirmation email to the customer
+    order['status'] = 'created'
+    order['merchant_reference'] = {
+        'orderid1': uuid1()
+    }
+    order.update(connector)
