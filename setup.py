d7995f093472633855eb066a79acb757e33f725e
Rickard Dybeck <rickard.dybeck@klarna.com>
2013-01-14 09:20:28 +0100

    Add Readme text

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup
 
 setup(
     name='KlarnaCheckout',
-    version='1.0.0dev',
+    version='1.0.0',
     packages=['klarnacheckout'],
     license='Apache 2',
-    long_description=open('README.txt').read(),
+    long_description=open('README.md').read(),
     author='Merchant Integration',
     author_email='support@klarna.com',
-    url='http://integration.klarna.com'
+    url='http://docs.klarna.com'
 )
