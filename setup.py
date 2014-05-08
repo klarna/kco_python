a740b93bab31a0e6c494a58bf7e7bbfe8cef7449
Rickard Dybeck <rickard.dybeck@klarna.com>
2014-05-08 14:18:18 +0200

    Bump version to 1.0.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='KlarnaCheckout',
-    version='1.0.0',
+    version='1.0.1',
     packages=['klarnacheckout'],
     license='Apache 2',
     description='Klarna Checkout library',
