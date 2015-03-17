776e4fb782b4b974bd19ef260e107b9d875b1207
David Keijser <david.keijser@klarna.com>
2015-03-17 10:49:42 +0100

    v1.0.2

--- klarnacheckout/useragent.py
+++ klarnacheckout/useragent.py
@@ -20,7 +20,7 @@ Additional fields can be added with the formatting taken care of.
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-__version__ = '1.0.1'
+__version__ = '1.0.2'
 
 import sys
 import os
