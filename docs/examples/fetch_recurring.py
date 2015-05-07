0e6f2e17d6e871e8307ebb603fb4e1bc2259c1d4
David Keijser <david.keijser@klarna.com>
2015-05-07 14:59:01 +0200

    add examples for recurring orders
    MINT-2132

---
+++ docs/examples/fetch_recurring.py
@@ -0,0 +1,33 @@
+'''Fetch recurring order example
+
+This file demonstrates the use of the Klarna library to fetch the status of a
+recurring order.
+'''
+
+# Copyright 2013 Klarna AB
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import klarnacheckout
+
+# Shared Secret
+shared_secret = 'shared_secret'
+
+# Recurring order token
+token = 'abc123'
+
+connector = klarnacheckout.create_connector(shared_secret,
+                                            klarnacheckout.BASE_TEST_URL)
+
+order = klarnacheckout.RecurringStatus(connector, token)
+order.fetch()
